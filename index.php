0a3a9889df1fe47ac9d1149767d62a713fcc9887
NjaratianaRB <njaratianarabe@gmail.com>
2023-09-10 12:50:32 +0300

    Version 2( +ergonomie

--- index.php
+++ index.php
@@ -23,40 +23,54 @@ require "elements/header.php";
     <h1>Livre d'Or</h1>
 
     <?php if (!empty($errors)): ?>
-    <div class="alert alert-danger">
-        Formulaire invalide
-    </div>
+        <div class="alert alert-danger">
+            Formulaire invalide
+        </div>
     <?php endif ?>
 
     <?php if ($sucess): ?>
-    <div class="alert alert-sucess">
-        Merci pour votre message
-    </div>
+        <div class="alert alert-success">
+            Merci pour votre message
+        </div>
     <?php endif ?>
 
     <form action="" method="post">
         <div class="form-group">
-            <input value="<?=htmlentities($_POST['username'] ?? "" )?>" type="text" name="username" placeholder="Votre Pseudo" id="username" class="form-control <?= isset($errors['username']) ? 'is-invalid' : '' ?> ">
-            <?php if (isset($errors['username'])):?>
-            <div class="invalid-feedback"><?= $errors['username']?></div>
-               <?php endif ?>
+            <input 
+                value="<?= htmlentities($_POST['username'] ?? '') ?>" 
+                type="text" 
+                name="username" 
+                placeholder="Votre Pseudo" 
+                id="username" 
+                class="form-control <?= isset($errors['username']) ? 'is-invalid' : '' ?> ">
+            <?php if (isset($errors['username'])): ?>
+                <div class="invalid-feedback"><?= $errors['username'] ?></div>
+            <?php endif ?>
         </div>
         <div class="form-group">
-            <textarea name="message" id="message" cols="20" rows="10" placeholder="Votre Message" class="form-control <?= isset($errors['message']) ? 'is-invalid' : '' ?> "><?= htmlentities($_POST['message'] ?? '')?></textarea>
-            <?php if (isset($errors['message'])):?>
-            <div class="invalid-feedback"><?= $errors['message']?></div>
-               <?php endif ?>
+            <textarea 
+                name="message" 
+                id="message" 
+                cols="20" 
+                rows="10" 
+                placeholder="Votre Message" 
+                class="form-control <?= isset($errors['message']) ? 'is-invalid' : '' ?>"
+            ><?= htmlentities($_POST['message'] ?? '') ?></textarea>
+            <?php if (isset($errors['message'])): ?>
+                <div class="invalid-feedback"><?= $errors['message'] ?></div>
+            <?php endif ?>
         </div>
-        <button class="btn btn primary">Envoyer</button>
+        <button type="submit" class="btn btn-primary">Envoyer</button>
     </form>
 
     <?php if (!empty($message)): ?>
-    <h1 class="mt-4">Vos messages</h1>
-    <?php foreach($message as $messages):?>
-       <?= $messages->toHTML();?>
-    <?php endforeach ?>
+        <h1 class="mt-4">Vos messages</h1>
+        <?php foreach($message as $messages): ?>
+            <?= $messages->toHTML() ?>
+        <?php endforeach ?>
     <?php endif ?>
 </div>
+
 <?php 
 require "elements/footer.php";
 ?>
